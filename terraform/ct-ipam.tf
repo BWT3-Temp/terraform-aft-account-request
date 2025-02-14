29c7f6fcad9320978afbdca0d081e74e9ecf3697
206719408-BWT3 <ian.mcmahon@cnbc.com>
2025-02-14 15:53:06 -0500

    rebuild with correct OU Name format
    Original format of just OUName failed during the product launch in SC, needed to terminate the product, and relaunch

--- terraform/ct-ipam.tf
+++ terraform/ct-ipam.tf
@@ -1,25 +1,25 @@
-module "ct-ipam" {
-  source = "./modules/aft-account-request"
+# module "ct-ipam" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "devops-ct-ipam@bwt3.com"
-    AccountName               = "ct-ipam"
-    ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
-    SSOUserEmail              = "devops-admin@BWT3.com"
-    SSOUserFirstName          = "devops"
-    SSOUserLastName           = "admin"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "devops-ct-ipam@bwt3.com"
+#     AccountName               = "ct-ipam"
+#     ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
+#     SSOUserEmail              = "devops-admin@BWT3.com"
+#     SSOUserFirstName          = "devops"
+#     SSOUserLastName           = "admin"
+#   }
 
-  account_tags = {
-    name                   = "ct-ipam"
-    workload_environment   = "prod"
-  }
+#   account_tags = {
+#     name                   = "ct-ipam"
+#     workload_environment   = "prod"
+#   }
 
-  # change_management_parameters = {}
+#   # change_management_parameters = {}
 
-  custom_fields = {
-    workload_environment   = "prod"
-  }
+#   custom_fields = {
+#     workload_environment   = "prod"
+#   }
 
-  account_customizations_name = "ct-ipam"
-}
+#   account_customizations_name = "ct-ipam"
+# }
