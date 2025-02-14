612780eb306e280c4aa8ef12490378fcb87f58d2
206719408-BWT3 <ian.mcmahon@cnbc.com>
2025-02-14 15:57:25 -0500

    Rebuild accounts with correct OU format

--- terraform/ct-root-mgmt.tf
+++ terraform/ct-root-mgmt.tf
@@ -1,25 +1,25 @@
-# module "ct-root-mgmt" {
-#   source = "./modules/aft-account-request"
+module "ct-root-mgmt" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail              = "devops-ct-rootmgmt@bwt3.com"
-#     AccountName               = "ct-root-mgmt"
-#     ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
-#     SSOUserEmail              = "devops-admin@BWT3.com"
-#     SSOUserFirstName          = "devops"
-#     SSOUserLastName           = "admin"
-#   }
+  control_tower_parameters = {
+    AccountEmail              = "devops-ct-rootmgmt@bwt3.com"
+    AccountName               = "ct-root-mgmt"
+    ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
+    SSOUserEmail              = "devops-admin@BWT3.com"
+    SSOUserFirstName          = "devops"
+    SSOUserLastName           = "admin"
+  }
 
-#   account_tags = {
-#     name                   = "ct-root-mgmt"
-#     workload_environment   = "prod"
-#   }
+  account_tags = {
+    name                   = "ct-root-mgmt"
+    workload_environment   = "prod"
+  }
 
-#   # change_management_parameters = {}
+  # change_management_parameters = {}
 
-#   custom_fields = {
-#     workload_environment   = "prod"
-#   }
+  custom_fields = {
+    workload_environment   = "prod"
+  }
 
-#   account_customizations_name = "ct-root-mgmt"
-# }
+  account_customizations_name = "ct-root-mgmt"
+}
