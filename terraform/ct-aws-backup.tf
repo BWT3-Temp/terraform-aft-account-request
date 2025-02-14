be62075d741371ebcc3d1f86f0d3a621a6907dcf
206719408-BWT3 <ian.mcmahon@cnbc.com>
2025-02-14 17:24:00 -0500

    last one v2
    final push to create the accounts

--- terraform/ct-aws-backup.tf
+++ terraform/ct-aws-backup.tf
@@ -1,25 +1,25 @@
-# module "ct-aws-backup" {
-#   source = "./modules/aft-account-request"
+module "ct-aws-backup" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail              = "devops-ct-backup@bwt3.com"
-#     AccountName               = "ct-aws-backup"
-#     ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
-#     SSOUserEmail              = "devops-admin@BWT3.com"
-#     SSOUserFirstName          = "devops"
-#     SSOUserLastName           = "admin"
-#   }
+  control_tower_parameters = {
+    AccountEmail              = "devops-ct-backup@bwt3.com"
+    AccountName               = "ct-aws-backup"
+    ManagedOrganizationalUnit = "Governance (ou-yr92-3lnpifxb)"
+    SSOUserEmail              = "devops-admin@BWT3.com"
+    SSOUserFirstName          = "devops"
+    SSOUserLastName           = "admin"
+  }
 
-#   account_tags = {
-#     name                   = "ct-aws-backup"
-#     workload_environment   = "prod"
-#   }
+  account_tags = {
+    name                   = "ct-aws-backup"
+    workload_environment   = "prod"
+  }
 
-#   # change_management_parameters = {}
+  # change_management_parameters = {}
 
-#   custom_fields = {
-#     workload_environment   = "prod"
-#   }
+  custom_fields = {
+    workload_environment   = "prod"
+  }
 
-#   account_customizations_name = "ct-aws-backup"
-# }
+  account_customizations_name = "ct-aws-backup"
+}
